f2b2bf6d3c48ad10206589d1092c77517b458813
Noah Guerin <noah.guerin@student.kdg.be>
2024-04-04 17:22:04 +0200

    Add create buttons to web pages

--- Team_5_Phygital/src/main/java/be/kdg/team_5_phygital/controller/mvc/AdminController.java
+++ Team_5_Phygital/src/main/java/be/kdg/team_5_phygital/controller/mvc/AdminController.java
@@ -35,7 +35,7 @@ public class AdminController {
     }
 
     @GetMapping("platform")
-    public String getPlatformPage(Model model) {
+    public String getPlatform(Model model) {
         List<SharingPlatform> platformList = sharingPlatformService.getAllSharingPlatforms();
         model.addAttribute("platforms", platformList);
         return "admin/platform";
@@ -43,7 +43,7 @@ public class AdminController {
 
 
     @GetMapping("sharing-platform/{platformId}")
-    public String getSharingPlatformPage(@PathVariable int platformId, Model model) {
+    public String getSharingPlatform(@PathVariable int platformId, Model model) {
         SharingPlatform sharingPlatform = sharingPlatformService.getSharingPlatformById(platformId).orElse(null);
         SharingPlatformAdmin client = sharingPlatformAdminService.findAllSharingPlatformAdmins().get(0);
         List<Project> projectList = projectService.getProjectBySharingPlatform(sharingPlatform);
@@ -55,25 +55,25 @@ public class AdminController {
         return "admin/sharing-platform";
     }
 
+    @GetMapping("sharing-platform/new")
+    public String getNewSharingPlatform() {
+        return "admin/new-sharing-platform";
+    }
+
     @GetMapping("sharing-platform/{platformId}/client/{clientId}")
-    public String getClientPage(@PathVariable int platformId, @PathVariable int clientId, Model model) {
+    public String getClient(@PathVariable int platformId, @PathVariable int clientId, Model model) {
         SharingPlatformAdmin client = sharingPlatformAdminService.findAllSharingPlatformAdmins().get(0);
         model.addAttribute("client", client);
         return "admin/client";
     }
 
     @GetMapping("sharing-platform/{platformId}/stats")
-    public String getPlatformStatsPage(@PathVariable int platformId, Model model) {
+    public String getPlatformStats(@PathVariable int platformId, Model model) {
         SharingPlatform sharingPlatform = sharingPlatformService.getSharingPlatformById(platformId).orElse(null);
         Project project = projectService.getAllProjects().stream().findFirst().orElse(null);
         model.addAttribute("platform", sharingPlatform);
         model.addAttribute("project", project);
         return "admin/sharing-platform-stats";
-    }
-
-    @GetMapping("sharing-platform/{platformId}/{projectId}/stats")
-    public String getProjectStatsPage(@PathVariable int platformId, @PathVariable int projectId) {
-        return "admin/project-stats";
     }
 
     @GetMapping("{platformId}/project/{projectId}")
@@ -85,6 +85,11 @@ public class AdminController {
         return "admin/project";
     }
 
+    @GetMapping("{platformId}/project/new")
+    public String getNewProject(@PathVariable int platformId) {
+        return "admin/new-project";
+    }
+
     @GetMapping("project/{projectId}/theme")
     public String getTheme(@PathVariable int projectId, Model model) {
         Theme theme = themeService.getThemeByProjectId(projectId);
@@ -92,8 +97,13 @@ public class AdminController {
         return "admin/theme";
     }
 
+    @GetMapping("sharing-platform/{platformId}/{projectId}/stats")
+    public String getProjectStats(@PathVariable int platformId, @PathVariable int projectId) {
+        return "admin/project-stats";
+    }
+
     @GetMapping("project/{projectId}/flow/{flowId}")
-    public String getFlow(@PathVariable int flowId, Model model) {
+    public String getFlow(@PathVariable int projectId, @PathVariable int flowId, Model model) {
         Flow flow = flowService.getFlowById(flowId).orElse(null);
         List<SubTheme> subThemes = subThemeService.getSubthemeByFlowId(flow);
         model.addAttribute("flow", flow);
@@ -101,13 +111,23 @@ public class AdminController {
         return "admin/flow";
     }
 
-    @GetMapping("flow/{flowId}/sub-theme/{stId}")
-    public String getSubTheme(@PathVariable int stId, Model model) {
-        SubTheme subTheme = subThemeService.getSubthemeById(stId).orElse(null);
+    @GetMapping("project/{projectId}/flow/new")
+    public String getNewFlow(@PathVariable int projectId) {
+        return "admin/new-flow";
+    }
+
+    @GetMapping("flow/{flowId}/sub-theme/{subThemeId}")
+    public String getSubTheme(@PathVariable int flowId, @PathVariable int subThemeId, Model model) {
+        SubTheme subTheme = subThemeService.getSubthemeById(subThemeId).orElse(null);
         List<Question> questions = questionService.getQuestionBySubTheme(subTheme);
         model.addAttribute("st", subTheme);
         model.addAttribute("question", questions);
         return "admin/sub-theme";
+    }
+
+    @GetMapping("flow/{flowId}/sub-theme/new")
+    public String getNewSubTheme(@PathVariable int flowId) {
+        return "admin/new-sub-theme";
     }
 
     @GetMapping("sub-theme/{stId}/question/{questionId}")
@@ -117,10 +137,20 @@ public class AdminController {
         return "admin/question";
     }
 
+    @GetMapping("sub-theme/{subThemeId}/question/new")
+    public String getNewQuestion(@PathVariable int subThemeId) {
+        return "admin/new-question";
+    }
+
     @GetMapping("{platformId}/supervisor/{supervisorId}")
     public String getSupervisor(@PathVariable int platformId, @PathVariable int supervisorId, Model model) {
         model.addAttribute("sv", supervisorService.getSupervisorById(supervisorId).orElse(null));
         return "admin/supervisor";
+    }
+
+    @GetMapping("{platformId}/supervisor/new")
+    public String getNewSupervisor(@PathVariable int platformId) {
+        return "admin/new-supervisor";
     }
 
     @GetMapping("project/{projectId}/stats")
