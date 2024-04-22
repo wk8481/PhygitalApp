230710c7852e9bc43e713726f214992f90302c1f
Noah Guerin <noah.guerin@student.kdg.be>
2024-04-22 10:54:21 +0200

    Try to fix POST, PATCH and DELETE

--- Team_5_Phygital/src/main/resources/static/js/admin/add-flow.js
+++ Team_5_Phygital/src/main/resources/static/js/admin/add-flow.js
@@ -1,3 +1,5 @@
+import {header, token} from "../util/csrf.js";
+
 const createButton = document.getElementById("createButton");
 const projectId = extractIdsFromUrl(window.location.href.substring(window.location.href), "flow");
 
@@ -7,7 +9,7 @@ async function addNewSubTheme() {
     const nameInput = document.getElementById("nameInput").value;
     await fetch(`/api/flows`, {
         method: "POST", headers: {
-            "Accept": "application/json", "Content-Type": "application/json"
+            'Accept': 'application/json', [header]: token
         }, body: JSON.stringify({
             name: nameInput, projectId: projectId
         })
