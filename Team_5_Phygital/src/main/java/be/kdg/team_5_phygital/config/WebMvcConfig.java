5c2a70c5a932ef039abd932077d858bfb85322b1
William Kasasa <william.kasasa@student.kdg.be>
2024-05-04 23:16:01 +0200

    maybe solved my issue with mvcresolver

---
+++ Team_5_Phygital/src/main/java/be/kdg/team_5_phygital/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package be.kdg.team_5_phygital.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Disable WebJars resource resolver
+        registry
+                .addResourceHandler("/webjars/**")
+                .resourceChain(false);
+    }
+}
