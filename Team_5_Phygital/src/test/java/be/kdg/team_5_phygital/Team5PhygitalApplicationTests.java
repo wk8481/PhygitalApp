b0122a4ad8a15bf08bfe46ec0ada9334fb453802
William Kasasa <william.kasasa@student.kdg.be>
2024-05-06 11:01:14 +0200

    context loads should work now

--- Team_5_Phygital/src/test/java/be/kdg/team_5_phygital/Team5PhygitalApplicationTests.java
+++ Team_5_Phygital/src/test/java/be/kdg/team_5_phygital/Team5PhygitalApplicationTests.java
@@ -2,8 +2,10 @@ package be.kdg.team_5_phygital;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Team5PhygitalApplicationTests {
 
     @Test
