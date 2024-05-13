20a1f4e99f729b736e66b99433ef913b179fc4be
Noah Guerin <noah.guerin@student.kdg.be>
2024-05-13 14:38:46 +0200

    Add projects to web app

--- Team_5_Phygital/src/main/java/be/kdg/team_5_phygital/controller/mvc/WebAppController.java
+++ Team_5_Phygital/src/main/java/be/kdg/team_5_phygital/controller/mvc/WebAppController.java
@@ -1,22 +1,32 @@
 package be.kdg.team_5_phygital.controller.mvc;
 
 
+import be.kdg.team_5_phygital.domain.Project;
+import be.kdg.team_5_phygital.service.ProjectService;
+import org.springframework.ui.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("web-app")
 public class WebAppController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public WebAppController() {
+    private final ProjectService projectService;
+
+    public WebAppController(ProjectService projectService) {
+        this.projectService = projectService;
     }
 
     @GetMapping("home")
-    public String getHome() {
+    public String getHome(Model model) {
+        List<Project> projects = projectService.getAllProjects();
+        model.addAttribute("projects", projects);
         return "web-app/home";
     }
 
