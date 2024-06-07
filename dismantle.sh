705b8a0a91df2c7a04442548456f2a88e6cfebca
William Kasasa <william.kasasa@student.kdg.be>
2024-06-07 15:23:21 +0200

    testo for destroyd without sudo n wit zone

--- dismantle.sh
+++ dismantle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Author: William
-# Script Name: stop_services.sh
+# Script Name: dismantle.sh
 # Description: This script stops a VM and a Cloud SQL instance on Google Cloud Platform (GCP).
 #              It removes the VM's IP address from the authorized networks of the Cloud SQL instance,
 #              then stops the VM and sets the Cloud SQL instance's activation policy to NEVER.
@@ -10,12 +10,13 @@
 remove_vm_ip_from_sql_authorized_networks() {
     local cloud_sql_instance="$1"
     local vm_instance_name="$2"
+    local zone="$3"
 
     # Fetch the current authorized networks for the Cloud SQL instance
     local AUTHORIZED_NETWORKS=$(gcloud sql instances describe "$cloud_sql_instance" --format="value(settings.ipConfiguration.authorizedNetworks)")
 
     # Fetch the IP address associated with the VM instance
-    local VM_IP=$(gcloud compute instances describe "$vm_instance_name" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
+    local VM_IP=$(gcloud compute instances describe "$vm_instance_name" --zone="$zone" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
 
     # Check if VM IP is obtained successfully
     if [ -z "$VM_IP" ]; then
@@ -38,10 +39,11 @@ remove_vm_ip_from_sql_authorized_networks() {
 # Function to stop the VM
 stop_vm() {
     local vm_name="$1"
+    local zone="$2"
 
     # Stop the VM
     echo "Stopping VM $vm_name..."
-    if ! gcloud compute instances stop "$vm_name" --quiet; then
+    if ! gcloud compute instances stop "$vm_name" --zone="$zone" --quiet; then
         echo "Failed to stop VM $vm_name"
         exit 1
     fi
@@ -63,14 +65,17 @@ stop_cloud_sql_instance() {
     echo "Cloud SQL instance $cloud_sql_instance successfully stopped"
 }
 
-# Variables for firewall rule
+# Variables
+CLOUD_SQL_INSTANCE="pg1716984918"
+VM_INSTANCE_NAME="team5-vm"
+ZONE="europe-west1-d"
 FIREWALL_RULE_NAME="http-https-postgresql-firewall"
 
 # Call the function to remove VM's IP from Cloud SQL authorized networks
-remove_vm_ip_from_sql_authorized_networks "pg1716984918" "team5-vm"
+remove_vm_ip_from_sql_authorized_networks "$CLOUD_SQL_INSTANCE" "$VM_INSTANCE_NAME" "$ZONE"
 
 # Call the function to stop the VM
-stop_vm "team5-vm"
+stop_vm "$VM_INSTANCE_NAME" "$ZONE"
 
 # Call the function to stop the Cloud SQL instance
-stop_cloud_sql_instance "pg1716984918"
+stop_cloud_sql_instance "$CLOUD_SQL_INSTANCE"
