5522e6134ff78a86cd28548bf645e31acc276de6
Noah Guerin <noah.guerin@student.kdg.be>
2024-06-09 14:04:33 +0200

    Fix theme media URL

--- src/main/js/admin/theme.js
+++ src/main/js/admin/theme.js
@@ -1,8 +1,6 @@
 import {header, token} from '../util/csrf.js'
 import { extractIdsFromUrl } from '../utils.js' // Adjust the path as per your file structure
 
-
-
 const submitButton = document.querySelector('#saveButton')
 const projectId = extractIdsFromUrl(window.location.href.substring(window.location.href), 'project')
 
@@ -11,18 +9,31 @@ submitButton.addEventListener('click', updateTheme)
 async function updateTheme(event) {
     const name = document.getElementById('nameInput').value
     const info = document.getElementById('infoInput').value
+    const mediaUrl = document.getElementById('mediaUrlInput').value;
 
-    console.log('updating sub theme to ' + name + ' and its info to ' + info)
-    fetch('/api/themes/{projectId}', {
+    console.log("Updating theme")
+
+    const body = {
+        id: projectId, name: name, information: info, mediaUrl: mediaUrl
+    };
+
+    if (mediaUrl) {
+        body.mediaUrl = mediaUrl;
+    }
+
+    fetch(`/api/themes/${projectId}`, {
         method: 'PATCH', headers: {
-            'Accept': 'application/json', [header]: token
-        }, body: JSON.stringify({
-            'id': projectId, 'name': name, 'information': info
-        })
+            'Accept': 'application/json', 'Content-Type': 'application/json', [header]: token
+        }, body: JSON.stringify(body)
     })
-        .then(response => {
+        .then(async (response) => {
             if (response.status === 204) {
-
+                console.log('Theme updated successfully');
+            } else {
+                console.error('Failed to update theme');
             }
         })
+        .catch(error => {
+            console.error('Error updating theme:', error);
+        });
 }
